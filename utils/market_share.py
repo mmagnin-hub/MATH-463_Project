abdfb93499d82984f99a43b27138c68ffb6c1e9e
Mathis Magnin <mathis.magnin@epfl.ch>
2024-12-20 14:03:17 +0100

    Can't do better

--- utils/market_share.py
+++ utils/market_share.py
@@ -6,22 +6,17 @@ import biogeme.database as db
 from biogeme.biogeme import BIOGEME
 from biogeme.expressions import Expression
 from biogeme.expressions import Variable
-from biogeme.models import loglogit, logit, nested
+from biogeme.models import loglogit, logit, nested, lognested
 
-
-# models
-from models.logit_lmpc12_model0 import V_0, logprob_0, biogeme_0, results as res_mod0, chosen_alternative
-from models.logit_lmpc12_model1 import V_1, logprob_1, biogeme_1, results as res_mod1
-from models.logit_lmpc12_model2 import V_2, logprob_2, biogeme_2, results as res_mod2
-from models.logit_lmpc12_model3 import V_3, logprob_3, biogeme_3, results as res_mod3
-from models.logit_lmpc12_model4 import results_nested, lognested, biogeme_nested, nests
+# models pref
+from models.logit_lmpc12_model3 import V_3, chosen_alternative
+from models.logit_lmpc12_model4 import results_nested, nests
 
 # forecasting
-from models_forecasting.logit_lmpc12_model_scenario1_boxcox import V_3 as V_scenario1, logprob_3 as logprob_scenario1,biogeme_3 as biogeme_scenario1, results as res_scenario1
-from models_forecasting.logit_lmpc12_model_scenario1_nested import results_nested as results_scenario1, lognested as lognested_scenario1, biogeme_nested as biogeme_scenario1
-from models_forecasting.logit_lmpc12_model_scenario2_boxcox import V_3 as V_scenario2, logprob_3 as logprob_scenario2,biogeme_3 as biogeme_scenario2, results as res_scenario2
-from models_forecasting.logit_lmpc12_model_scenario2_nested import results_nested as results_scenario2, lognested as lognested_scenario2, biogeme_nested as biogeme_scenario2
-
+from models_forecasting.logit_lmpc12_model_scenario1_boxcox import V_3 as V_scenario1
+from models_forecasting.logit_lmpc12_model_scenario1_nested import results_nested as results_scenario1
+from models_forecasting.logit_lmpc12_model_scenario2_boxcox import V_3 as V_scenario2
+from models_forecasting.logit_lmpc12_model_scenario2_nested import results_nested as results_scenario2
 
 # Load the dataset
 df = pd.read_csv('models/lpmc12.dat', sep='\t')
@@ -81,31 +76,26 @@ class IndicatorTuple(NamedTuple):
     lower: float
     upper: float
 
-def market_share(utilities: dict[int, Expression], results, biogeme_model = None, is_nested = False) -> dict[str, IndicatorTuple]:
+def market_share(utilities: dict[int, Expression], results) -> dict[str, IndicatorTuple]:
     """Calculate the market shares of all alternatives, given the
     specification of the utility functions.
 
-    :param utilities: Specification of the utility functions. It is a
+    :param utilities: specification of the utility functions. It is a
         dict where the keys are the IDs of the alternatives, and the
         values are the expressions of the utility functions.
 
-    :return: A dictionary where each entry corresponds to an
+    :return: a dictionary where each entry corresponds to an
         alternative, and associates its name with the IndicatorTuple
         containing the value of the market share, and the lower and
         upper bounds of the 90% confidence interval.
-    """
-    if is_nested:
-        prob_walk = nested(utilities, None, nests, 1)
-        prob_cycle = nested(utilities, None, nests, 2)
-        prob_pt = nested(utilities, None, nests, 3)
-        prob_car = nested(utilities, None, nests, 4)
-    else:
-        prob_walk = logit(utilities, None, 1)
-        prob_cycle = logit(utilities, None, 2)
-        prob_pt = logit(utilities, None, 3)
-        prob_car = logit(utilities, None, 4)
 
-    # Simulation setup
+
+    """
+    prob_walk = nested(utilities, None, nests, 1)
+    prob_cycle = nested(utilities, None, nests, 2)
+    prob_pt = nested(utilities, None, nests, 3)
+    prob_car = nested(utilities, None, nests, 4)
+
     simulate = {
         'weight': normalizedWeight,  # Assuming normalized weights are provided
         'Prob. walk': prob_walk,
@@ -113,23 +103,20 @@ def market_share(utilities: dict[int, Expression], results, biogeme_model = None
         'Prob. PT': prob_pt,
         'Prob. car': prob_car,
     }
-    
+
     # Creating Biogeme object
     biosim = BIOGEME(database, simulate)
     simulated_values = biosim.simulate(results.get_beta_values())
     print(simulated_values[['Prob. walk', 'Prob. cycle', 'Prob. PT', 'Prob. car']].describe())
-    
-    # Confidence intervals
-    if is_nested:
-        betas = biogeme_model.free_beta_names
-    else:
-        logprob = loglogit(utilities, None, chosen_alternative)
-        biogeme = BIOGEME(database, logprob)
-        betas = biogeme.free_beta_names
+
+    logprob = lognested(utilities, None, nests, chosen_alternative)
+    biogeme = BIOGEME(database, logprob)
+    betas = biogeme.free_beta_names
     sensitivity_betas = results.get_betas_for_sensitivity_analysis(
         betas, use_bootstrap=False
     )
     left, right = biosim.confidence_intervals(sensitivity_betas, 0.9)
+
 
 
     # Initialize market shares
@@ -164,3 +151,4 @@ def market_share(utilities: dict[int, Expression], results, biogeme_model = None
         )
 
     return market_shares
+
